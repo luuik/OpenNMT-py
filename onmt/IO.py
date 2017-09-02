f65f3f47566fdeed81b42f46eb16a368896fb075
luuik <loic.dugast@gmail.com>
2017-09-02 10:08:47 +0100

    trying to fix tm_tgt batch tensor

--- onmt/IO.py
+++ onmt/IO.py
@@ -405,7 +405,9 @@ class TMNMTDataset(ONMTDataset):
             cat = [batch.__dict__["tm_src_"+str(k)][0]]
             cat = [c.unsqueeze(2) for c in cat]
             src = torch.cat(cat, 2)
-            tgt = batch.__dict__["tm_tgt_"+str(k)][1]
+            cat = [batch.__dict__["tm_tgt_"+str(k)]]
+            cat = [c.unsqueeze(2) for c in cat]
+            tgt = torch.cat(cat, 2)
             tm_src.append(src)
             tm_tgt.append(tgt)
         return tm_src, tm_tgt
